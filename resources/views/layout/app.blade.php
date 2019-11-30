dce64ea2259a95c43cc8d034decc09913215a808
Ruka <lukamenda26@gmail.com>
2019-11-30 18:16:26 +0900

    modified app.blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,6 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Bootstrap本体 -->
         <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
-        <script type="text/javascript" src="{{ asset('/js/index.js') }}"></script>
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -28,4 +27,5 @@
             @yield('content')
         </div>
     </body>
+    <script type="text/javascript" src="{{ asset('/js/index.js') }}"></script>
 </html>
