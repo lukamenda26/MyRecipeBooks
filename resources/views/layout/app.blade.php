0aed4ebf711eb027bc8e87d53246b5c56d637821
Ruka <lukamenda26@gmail.com>
2019-12-05 01:28:01 +0900

    using laravel mix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,14 +3,12 @@
         <meta charset='utf-8'>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>料理本おすすめサイト Myレシピブック - @yield('title')</title>
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.css') }}"><!-- Bootstrap本体 -->
         <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
     </head>
     <body>
     <div class="container">
@@ -26,6 +24,11 @@
         </p>
             @yield('content')
         </div>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script> 
     </body>
 
 </html>
