941701a4a31d8cd082efcfed65d5f8f5b8999f95
Ruka <lukamenda26@gmail.com>
2020-03-08 16:35:26 +0900

    Delete unused codes

--- resources/assets/js/components/index.vue
+++ resources/assets/js/components/index.vue
@@ -15,27 +15,3 @@
         </div>
     </div>
 </template>
-
-<script>
- var vue = new Vue({
-  el: "#vue",
-  data: {
-      score: 5
-  },
-  computed: {
-      showStar: function() {
-          if (this.score == 5) {
-              return "★★★★★"
-          } else if (this.score == 4) {
-              return "★★★★"
-          } else if (this.score == 3) {
-              return "★★★"
-          } else if (this.score == 2) {
-              return "★★"
-          } else {
-              return "★"
-          }
-      }
-  }
-})
-</script>
