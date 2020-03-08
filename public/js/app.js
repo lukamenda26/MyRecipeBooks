ae7019f075d681e9347b079e8a1d1847af8fd675
Ruka <lukamenda26@gmail.com>
2020-03-08 17:52:07 +0900

    Delete unnecessary comment

--- public/js/app.js
+++ public/js/app.js
@@ -1191,40 +1191,6 @@ window.addEventListener('load', () => {
   })
 })
 
-// HTMLをloadしたタイミングで発火させるeventを設定
-// loadイベントを上書きするver.
-// jsがロードされるタイミングで
-// window.onload = judgeVueExisthing();
-
-// #vueのelementの存在をチェックする
-// function judgeVueExisthing() {
-//     const judge = document.getElementById("vue");
-//     if (judge == null) {
-//         return;
-//     } else {
-//         const vue = new Vue({
-//             el: '#vue',
-//             data: {
-//                 score: 5
-//             },
-//             computed: {
-//                 showStar: function() {
-//                     if (this.score == 5) {
-//                         return "★★★★★"
-//                     } else if (this.score == 4) {
-//                         return "★★★★"
-//                     } else if (this.score == 3) {
-//                         return "★★★"
-//                     } else if (this.score == 2) {
-//                         return "★★"
-//                     } else {
-//                         return "★"
-//                     }
-//                 }
-//             }
-//         })
-//     }
-// }
 
 /***/ }),
 /* 13 */
